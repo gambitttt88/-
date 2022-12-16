5bc05b93f408b9edf33646e824d1d64029664f2c
Sergey <gambitttt@mail.ru>
2022-12-16 15:47:53 +0300

    решил задачу 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int number1 = int.Parse(Console.ReadLine());
+
+int k = number / number1;
+if(k == number1)
+{
+   Console.WriteLine($"Число {number} является квадратом числа {number1}"); 
+}
+else
+{
+ Console.WriteLine($"Число {number} НЕ является квадратом числа {number1}");   
+}
